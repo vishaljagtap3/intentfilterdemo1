aa743f3aae10b8628d87cc544d2445916a58db49
vishaljagtap3 <vishal.jagtap@bitcode.in>
2020-11-02 10:02:23 +0530

    Intent Filter 1

---
+++ app/src/main/java/in/bitcode/intentfilterdemo1/ImageActivity.java
@@ -0,0 +1,22 @@
+package in.bitcode.intentfilterdemo1;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ImageActivity extends AppCompatActivity {
+
+    private ImageView mImg;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mImg = new ImageView(this);
+        mImg.setImageURI( Uri.parse( getIntent().getStringExtra("path") ) );
+        setContentView(mImg);
+    }
+}
---
+++ app/src/main/java/in/bitcode/intentfilterdemo1/MainActivity.java
@@ -0,0 +1,38 @@
+package in.bitcode.intentfilterdemo1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText mEdtFilePath;
+    private Button mBtnShowImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mBtnShowImage = findViewById(R.id.btnShowImage);
+        mEdtFilePath = findViewById(R.id.edtFilePath);
+
+        mBtnShowImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent("in.bitcode.media.image.VIEW");
+                intent.putExtra("path", mEdtFilePath.getText().toString());
+                startActivity(intent);
+
+            }
+        });
+
+    }
+}
